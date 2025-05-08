f9c55d7aecc79a651d856c10cb2e365c5396a0f2
n04nik <85937098+n04nik@users.noreply.github.com>
2025-05-08 15:30:38 +0300

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+/**
+  * Created by IntelliJ IDEA.<br/>
+  * User:user
+  * Date: 08.05.2025
+  * Time: 15:28
+  */
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("its a new one");
+    System.out.println("its a second one from master");
+
+  }
+}
+  
